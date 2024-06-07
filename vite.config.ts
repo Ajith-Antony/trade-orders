0fa55f6ea327c003198c36c02722f27ba3ba171b
AjithAntony <ajithpallisseryantony@gmail.com>
2024-06-07 16:44:38 +0400

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/trade-orders/",
   server: {
     open: true,
     host: "0.0.0.0",
